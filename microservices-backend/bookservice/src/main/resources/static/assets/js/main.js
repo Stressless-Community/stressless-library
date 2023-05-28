15be726aec1c10bcfcb338cd59a72a7a8c0fce9c
Bibuya Luvako <bibuya@inua.inua>
2023-05-28 17:04:56 +0200

    Revert "set up slider and some styles to the header"
    This reverts commit 36e2f075c9dd1fa3a546c776f2e8cfc8c3ba15f6, reversing
    changes made to c3eb288fc403a3bf168d3458b1fd7aa796bc034c.

--- microservices-backend/bookservice/src/main/resources/static/assets/js/main.js
+++ microservices-backend/bookservice/src/main/resources/static/assets/js/main.js
@@ -1,21 +1,11 @@
 
 var Swipes = new Swiper('.swiper-container', {
     loop: true,
-    centeredSlides: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
     },
-    autoplay:{
-        delay:3000,
-        disableOnInteraction: false,
-    }
 });
-
-let index_currentSlide = Swipes.realIndex;
-let currentSlide = Swipes.slides[index_currentSlide]
-
