7b4b1389cde013e044e4433e486edafc59ce2edc
Egide Mushoko <103942607+Owlsome001@users.noreply.github.com>
2023-05-29 08:10:19 +0100

    Update BookImageRepository.java

--- microservices-backend/bookservice/src/main/java/com/stresslesslibrary/bookservice/repositories/BookImageRepository.java
+++ microservices-backend/bookservice/src/main/java/com/stresslesslibrary/bookservice/repositories/BookImageRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.stresslesslibrary.bookservice.entities.Author;
 import com.stresslesslibrary.bookservice.entities.BookImage;
 
 @RepositoryRestResource
 public interface BookImageRepository extends JpaRepository<BookImage, Integer> {
 	
-	@Query(value="SELECT im  FROM BookImage im WHERE im.name=:search")
-	BookImage findByName(@Param("search") String search) throws Exception;
+	Boolean existsBookImageByName(String name);
+
+	BookImage findBookImageByName(String name);
 }
