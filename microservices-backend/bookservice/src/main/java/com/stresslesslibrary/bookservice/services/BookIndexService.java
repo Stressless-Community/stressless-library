7ef18348a13325ceb1345795abcd992663d91ca5
Egide Mushoko <103942607+Owlsome001@users.noreply.github.com>
2023-07-06 08:09:02 +0200

    Working on adding indexes in the book recording form

---
+++ microservices-backend/bookservice/src/main/java/com/stresslesslibrary/bookservice/services/BookIndexService.java
@@ -0,0 +1,12 @@
+package com.stresslesslibrary.bookservice.services;
+
+import java.util.List;
+
+import com.stresslesslibrary.bookservice.entities.BookIndex;
+
+public interface BookIndexService {
+    
+    List<BookIndex> findAll();
+
+    BookIndex getOne();
+}
